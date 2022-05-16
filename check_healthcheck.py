6bd121bf97ae6d1ee5c11ed7cd068ac5d8f5c697
Katarina Zailac <katarina.zailac@gmail.com>
2022-05-16 10:26:15 +0200

    Return critical status if values are not validated

--- check_healthcheck.py
+++ check_healthcheck.py
@@ -1,52 +1,56 @@
 #!/usr/bin/env python
-
-import os
-import sys
-import io
-import time
 import argparse
-import json
+import sys
+
 import requests
 
 
 def print_help():
-        """ Print help values."""
+    """ Print help values."""
 
-        print("usage: check_healthcheck.py -H -k")
-        print("--- ---- ---- ---- ---- ---- ----\n")
-        print("main arguments:")
-        print("-H hostname")
-        print("-k token")
-        print("\n")
-        print("optional arguments:")
-        print(" -h, --help  show this help message and exit")
-        print("-t timeout")
-        print("-v verbose")
-        print("-k token")
+    print("usage: check_healthcheck.py -H -k")
+    print("--- ---- ---- ---- ---- ---- ----\n")
+    print("main arguments:")
+    print("-H hostname")
+    print("-k token")
+    print("\n")
+    print("optional arguments:")
+    print(" -h, --help  show this help message and exit")
+    print("-t timeout")
+    print("-v verbose")
+    print("-k token")
+
 
 def ValidateValues(arguments):
-        """ Validate values - input values """
+    """ Validate values - input values """
 
-        if arguments.timeout <= 0:
-            print("\nInvalid timeout value: %s\n" % arguments.timeout)
-            print_help()
-            exit()
+    if arguments.timeout <= 0:
+        print("CRITICAL - Invalid timeout value: %s\n" % arguments.timeout)
+        print_help()
+        exit(2)
 
-        if arguments.hostname is None:
-            print("\nNo hostname provided\n")
-            print_help()
-            exit()
+    if arguments.hostname is None:
+        print("CRITICAL - No hostname provided\n")
+        print_help()
+        exit(2)
 
-        if arguments.token is None:
-            print("\nNo token is provided. Token is needed to fetch the healthcheck\n")
-            print_help()
-            exit()
+    if arguments.token is None:
+        print(
+            "CRITICAL - No token is provided. "
+            "Token is needed to fetch the healthcheck\n"
+        )
+        print_help()
+        exit(2)
 
-        if not arguments.hostname.startswith("http"):
-            print("\nNo schema supplied with hostname, did you mean https://%s?\n" % arguments.hostname)
-            print_help()
-            exit()
- 
+    if not arguments.hostname.startswith("http"):
+        print(
+                "CRITICAL - No schema supplied with hostname, "
+                "did you mean https://%s?\n" % arguments.hostname
+        )
+        print_help()
+        exit(2)
+
+
 def debugValues(arguments):
     """ Print debug values.
         Args:
@@ -60,15 +64,15 @@ def debugValues(arguments):
         print("[debugValues] - timeout: %s" % arguments.timeout)
     if arguments.debug != '':
         print("[debugValues] - verbose: %s" % arguments.debug)
-        
-def checkHealth(URL, timeout):
+
+
+def checkHealth(url, timeout):
     """ Check service status.
         Args:
-           URL : service hostname
+           url : service URL
            timeout : how long should we wait for a response from the server
     """
-    out = None
-    u = URL
+    u = url
     try:
         out = requests.get(url=u, timeout=timeout)
 
@@ -80,35 +84,31 @@ def checkHealth(URL, timeout):
         description = "CRITICAL - Service unreachable"
         exit_code = 2
         return description, exit_code
-      
+
     if out is None:
         description = "UNKNOWN - Status unknown"
         exit_code = 3
         return description, exit_code
 
-    if out.status_code != 200:
+    elif out.status_code != 200:
         description = "WARNING - Unexpected status code %s" % out.status_code
         exit_code = 1
         return description, exit_code
 
-    content = out.json()
-    health = out.json()['healthy']
-    message = out.json()['message']
-    issues = out.json()['issues']
-
-    if health is True:
-        description = "OK - Service reachable:  %s" % message
-        exit_code = 0
-        return description, exit_code
     else:
-        description = "CRITICAL - Unexpected response: %s"  % message
-        exit_code = 1
-        return description, exit_code
+        health = out.json()['healthy']
+        message = out.json()['message']
 
-    description = "OK - Service reachable"
-    exit_code = 0
-    return description, exit_code
-  
+        if health is True:
+            description = "OK - Service reachable:  %s" % message
+            exit_code = 0
+            return description, exit_code
+        else:
+            description = "CRITICAL - Unexpected response: %s" % message
+            exit_code = 1
+            return description, exit_code
+
+
 def printResult(description, exit_code):
     """ Print the predefined values
         Args:
@@ -119,32 +119,42 @@ def printResult(description, exit_code):
     print(description)
     sys.exit(exit_code)
 
-def main():
 
-    start = time.time()
-    parser = argparse.ArgumentParser(description='EOSC Helpdesk metric '
-                                                 'Supports healthcheck.')
-    parser.add_argument("--hostname", "-H", help='The Hostname of EOSC Helpdesk service')
-    parser.add_argument("--timeout", "-t", metavar="seconds", help="Timeout in seconds. Must be greater than zero", type=int, default=30)
-    parser.add_argument("--token", "-k", metavar="token", help="the token required to display the status")
-    parser.add_argument("--verbose", "-v", dest='debug', help='Set verbosity level', action='count', default=0)
+def main():
+    parser = argparse.ArgumentParser(
+        description='EOSC Helpdesk metric Supports healthcheck.'
+    )
+    parser.add_argument(
+        "--hostname", "-H", help='The Hostname of EOSC Helpdesk service'
+    )
+    parser.add_argument(
+        "--timeout", "-t", metavar="seconds", type=int, default=30,
+        help="Timeout in seconds. Must be greater than zero"
+    )
+    parser.add_argument(
+        "--token", "-k", metavar="token",
+        help="the token required to display the status"
+    )
+    parser.add_argument(
+        "--verbose", "-v", dest='debug', help='Set verbosity level',
+        action='count', default=0
+    )
     arguments = parser.parse_args()
     ValidateValues(arguments)
 
-    if arguments.debug > 0 :
+    if arguments.debug > 0:
         debugValues(arguments)
 
-    URL = arguments.hostname
-    URL = URL + 'api/v1/monitoring/health_check?token=' + arguments.token
-    description, exit_code = checkHealth(URL, arguments.timeout)
+    url = arguments.hostname
+    url = url + 'api/v1/monitoring/health_check?token=' + arguments.token
+    description, exit_code = checkHealth(url, arguments.timeout)
 
-    #time to run 
-    rta = time.time()-start
     # Health check failed, unable to continue
     if exit_code > 0:
         printResult(description, exit_code)
 
     printResult(description, exit_code)
+
 
 if __name__ == "__main__":
     main()
